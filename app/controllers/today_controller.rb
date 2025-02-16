96adf1af42df7c877ad09dd6f993b14682247f9a
Cory Spitzer <3945059+CorySpitzer@users.noreply.github.com>
2025-02-16 11:14:16 -0500

    Change Today to One Day Agenda

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,9 +2,18 @@ class TodayController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @current_date = if params[:date].present?
+      Date.parse(params[:date])
+    else
+      Date.current
+    end
+
+    @prev_date = @current_date - 1.day
+    @next_date = @current_date + 1.day
+
     @today_skills = current_user.skills
                                .joins(:practice_schedules)
-                               .where(practice_schedules: { scheduled_date: Date.current, completed: false })
+                               .where(practice_schedules: { scheduled_date: @current_date, completed: false })
                                .distinct
   end
 end
